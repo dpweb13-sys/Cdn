8af7180dded5722055f3c0bac0f3e39527fadc08
dpweb13-sys <dpweb13@gmail.com>
2025-10-08 06:46:05 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "8287854128:AAFSqgqVE6HqdVDTH_wBjUeA1lmY2BShXVU",
+  MONGO_URI: "mongodb+srv://mrrabbit:mrrabbit@cluster0.we4bxzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  VERCE_URL: "https://files-rabbit.vercel.app", // তোমার vercel link
+};
